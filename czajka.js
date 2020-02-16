73a97f3f3b9884b8126a54d44cf73ca77acbadcc
krystiano13 <krystiano13@interklasa.pl>
2020-02-16 10:42:21 +0100

    Nowe samoloty

--- czajka.js
+++ czajka.js
@@ -1,4 +1,8 @@
 
 const planeczajka = document.querySelector('#plane4');
+const p5 = document.querySelector('#plane5');
+const p6 = document.querySelector('#plane6');
 planeczajka.addEventListener('click',()=>window.location.href = 'Html/czajka.html');
+p5.addEventListener('click',()=>window.location.href='Html/Los.html');
+p6.addEventListener('click',()=>window.location.href='Html/Lublin.html');
 
