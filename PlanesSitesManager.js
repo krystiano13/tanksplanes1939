2f284f812e08700c1c240a181e5c99d7e3c803c9
krystiano13 <krystiano13@interklasa.pl>
2020-02-16 11:00:23 +0100

    naprawiona podstrona

--- PlanesSitesManager.js
+++ PlanesSitesManager.js
@@ -5,6 +5,6 @@ const planekaras = document.querySelector('#plane3');
 
 
 planepzlpz.addEventListener('click',()=>window.location.href = 'Html/pzlp.html');
-planepzlp11c.addEventListener('click',()=>window.location.href = 'Html/pzlp11.html');
+planepzlp11c.addEventListener('click',()=>window.location.href = 'Html/pzlpc.html');
 planekaras.addEventListener('click',()=>window.location.href = 'Html/karas.html');
 
